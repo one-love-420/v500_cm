e7070e38764610c92215894ff22a4dc04aef4e22
dr.beat <xgecco@gmail.com>
2014-07-07 11:44:09 +0200

    Revert "https://github.com/franciscofranco/mako/commit/14bcff8023a6219f50c3dea7bd7d00eb1fe6b453#diff-f9151c7d297c89adb328b669c158f3c3R229"
    This reverts commit c2ca91dbe32bd1c3f281388e36db592e461349ac.

--- drivers/gpu/msm/kgsl_pwrscale_trustzone.c
+++ drivers/gpu/msm/kgsl_pwrscale_trustzone.c
@@ -9,8 +9,6 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
- * Modified by Paul Reioux (Faux123)
- * 2013-06-20: Added KGSL Simple GPU Governor
  */
 
 #include <linux/export.h>
@@ -20,7 +18,6 @@
 #include <linux/spinlock.h>
 #include <mach/socinfo.h>
 #include <mach/scm.h>
-#include <linux/module.h>
 
 #include "kgsl.h"
 #include "kgsl_pwrscale.h"
@@ -28,7 +25,6 @@
 
 #define TZ_GOVERNOR_PERFORMANCE 0
 #define TZ_GOVERNOR_ONDEMAND    1
-#define TZ_GOVERNOR_SIMPLE	2
 
 struct tz_priv {
 	int governor;
@@ -83,8 +79,6 @@ static ssize_t tz_governor_show(struct kgsl_device *device,
 
 	if (priv->governor == TZ_GOVERNOR_ONDEMAND)
 		ret = snprintf(buf, 10, "ondemand\n");
-	else if (priv->governor == TZ_GOVERNOR_SIMPLE)
-		ret = snprintf(buf, 8, "simple\n");	
 	else
 		ret = snprintf(buf, 13, "performance\n");
 
@@ -108,8 +102,6 @@ static ssize_t tz_governor_store(struct kgsl_device *device,
 
 	if (!strncmp(str, "ondemand", 8))
 		priv->governor = TZ_GOVERNOR_ONDEMAND;
-	else if (!strncmp(str, "simple", 6))
-		priv->governor = TZ_GOVERNOR_SIMPLE;	
 	else if (!strncmp(str, "performance", 11))
 		priv->governor = TZ_GOVERNOR_PERFORMANCE;
 
@@ -139,53 +131,9 @@ static void tz_wake(struct kgsl_device *device, struct kgsl_pwrscale *pwrscale)
 {
 	struct tz_priv *priv = pwrscale->priv;
 	if (device->state != KGSL_STATE_NAP &&
-		(priv->governor == TZ_GOVERNOR_ONDEMAND||
-		 priv->governor == TZ_GOVERNOR_SIMPLE))
+		priv->governor == TZ_GOVERNOR_ONDEMAND)
 		kgsl_pwrctrl_pwrlevel_change(device,
 					device->pwrctrl.default_pwrlevel);
-}
-
-#define HISTORY_SIZE 10
-
-static int ramp_up_threshold = 5500;
-module_param_named(simple_ramp_threshold, ramp_up_threshold, int, 0664);
-
-static unsigned int history[HISTORY_SIZE] = {0};
-static unsigned int counter = 0;
-
-static int simple_governor(struct kgsl_device *device, int idle_stat)
-{
-	struct kgsl_pwrctrl *pwr = &device->pwrctrl;
-	int i;
-	unsigned int total = 0;
-    
-	history[counter] = idle_stat;
-    
-	for (i = 0; i < HISTORY_SIZE; i++)
-		total += history[i];
-    
-	total = total/HISTORY_SIZE;
-    
-	if (++counter == 10)
-		counter = 0;
-    
-	/* it's currently busy */
-	if (total < ramp_up_threshold)
-	{
-		if ((pwr->active_pwrlevel > 0) &&
-			(pwr->active_pwrlevel <= (pwr->num_pwrlevels - 1)))
-        /* bump up to next pwrlevel */
-			return -1;
-	}
-	/* idle case */
-	else
-	{
-		if ((pwr->active_pwrlevel >= 0) &&
-			(pwr->active_pwrlevel < (pwr->num_pwrlevels - 1)))
-			return 1;
-	}
-    
-	return 0;
 }
 
 static void tz_idle(struct kgsl_device *device, struct kgsl_pwrscale *pwrscale)
@@ -237,9 +185,7 @@ static void tz_idle(struct kgsl_device *device, struct kgsl_pwrscale *pwrscale)
 	/* If the decision is to move to a lower level, make sure the GPU
 	 * frequency drops.
 	 */
-	if (priv->governor == TZ_GOVERNOR_SIMPLE)
-		val = simple_governor(device, idle);
-	else if (val > 0)
+	if (val > 0)
 		val *= pwr->step_mul;
 	if (val)
 		kgsl_pwrctrl_pwrlevel_change(device,
