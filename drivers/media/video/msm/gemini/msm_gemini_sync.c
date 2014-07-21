1484dabf075411829a5fbcbf760a8364d9b1eb21
dr.beat <xgecco@gmail.com>
2014-07-21 11:33:13 +0200

    Revert "https://www.codeaurora.org/cgit/quic/la/kernel/msm/commit/?h=aosp-new/android-msm-mako-3.4-kitkat-mr2&id=ad23a994e093ecc0e63a6ffdf7a6a8155f010c01"
    This reverts commit 0c6f53a943b9ca13b9c0a9ad66caedcdd4353789.

--- drivers/media/video/msm/gemini/msm_gemini_sync.c
+++ drivers/media/video/msm/gemini/msm_gemini_sync.c
@@ -23,7 +23,6 @@
 #include <mach/msm_bus.h>
 #include <mach/msm_bus_board.h>
 
-#define UINT32_MAX    (4294967295U)
 static int release_buf;
 
 /* size is based on 4k page size */
@@ -805,7 +804,7 @@ int msm_gemini_ioctl_hw_cmds(struct msm_gemini_device *pgmn_dev,
 	void * __user arg)
 {
 	int is_copy_to_user;
-	uint32_t len;
+	int len;
 	uint32_t m;
 	struct msm_gemini_hw_cmds *hw_cmds_p;
 	struct msm_gemini_hw_cmd *hw_cmd_p;
@@ -813,13 +812,6 @@ int msm_gemini_ioctl_hw_cmds(struct msm_gemini_device *pgmn_dev,
 	if (copy_from_user(&m, arg, sizeof(m))) {
 		GMN_PR_ERR("%s:%d] failed\n", __func__, __LINE__);
 		return -EFAULT;
-	}
-
-	if ((m == 0) || (m > ((UINT32_MAX-sizeof(struct msm_gemini_hw_cmds))/
-		sizeof(struct msm_gemini_hw_cmd)))) {
-		GMN_PR_ERR("%s:%d] outof range of hwcmds\n",
-		__func__, __LINE__);
-		return -EINVAL;
 	}
 
 	len = sizeof(struct msm_gemini_hw_cmds) +
