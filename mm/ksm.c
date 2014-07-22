082041bcbdead858199c91e14d363632789ee8e9
dr.beat <xgecco@gmail.com>
2014-07-22 14:33:08 +0200

    ksm: revert

--- mm/ksm.c
+++ mm/ksm.c
@@ -189,12 +189,6 @@ static unsigned int ksm_thread_pages_to_scan = 100;
 /* Milliseconds ksmd should sleep between batches */
 static unsigned int ksm_thread_sleep_millisecs = 20;
 
-<<<<<<< HEAD
-/* Boolean to indicate whether to use deferred timer or not */
-static bool use_deferred_timer = true;
-
-=======
->>>>>>> parent of 1324d2a... ksm: Provide support to use deferred timers for scanner thread
 #define KSM_RUN_STOP	0
 #define KSM_RUN_MERGE	1
 #define KSM_RUN_UNMERGE	2
