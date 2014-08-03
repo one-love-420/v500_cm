10ca70b371d87d53e268588c30b8a33f18bc246d
dr.beat <xgecco@gmail.com>
2014-08-03 13:57:01 +0200

    build.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,19 @@
-echo "build zImage"
+echo "1. build zImage"
 make -j3
 
-echo "copy zImage to ~/android/boot/<"
-cp arch/arm/boot/zImage ~/android/boot/
+rm -f zip/kernel/zImage
+echo "2. copy zImage to zip/kernel/<"
+cp arch/arm/boot/zImage zip/kernel/
 
-echo "copy modules to ~/android/boot/modules/<"
-find ~/android/v500_cm -name "*.ko" -exec cp {} ~/android/boot/modules/ \;
+echo "3. copy modules to zip/system/lib/modules<"
+rm -r zip/system/lib/modules
+mkdir zip/system/lib/modules
+find -type d \( ! -name zip-type d \( ! -name tmp \) \) -name "*.ko" -exec cp {} zip/system/lib/modules/ \;
+
+zipfile="mani.zip"
+echo "4. build .zip"
+cd zip/
+rm -f *.zip
+zip -9 -r $zipfile *
+rm -f /tmp/*.zip
+cp *.zip /tmp
